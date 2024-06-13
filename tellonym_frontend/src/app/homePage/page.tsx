59dd9aa7b97b614dcbb8f62d264265adb49b4a62
khemraj9815 <02230286.cst@rub.edu.bt>
2024-06-13 14:00:01 +0600

    recommendation

--- tellonym_frontend/src/app/homePage/page.tsx
+++ tellonym_frontend/src/app/homePage/page.tsx
@@ -1,32 +1,50 @@
 import React from 'react';
 import Tell from "../../reuseable_component/hometells"; 
 import Header from "../../reuseable_component/homepageheader";
-import Navigation from "../../reuseable_component/navigation"
+import Navigation from "../../reuseable_component/navigation";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const HomePage = () => {
   return (
-    
     <div className="flex justify-center items-start h-screen bg-gray-100">
       <main className="flex flex-col items-center justify-start h-full max-w-md w-full p-4 border border-gray-300 bg-white shadow-md rounded-lg">
         <div>
           <Header />
         </div>
-        <Tell
-          message="message"
-          username="khem"
-          timeAgo="2 hours ago"
-          avatarUrl="/path/to/avatar.jpg"
-          replies="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        />
-        <Tell
-          message="message"
-          username="khem"
-          timeAgo="2 hours ago"
-          avatarUrl="/path/to/avatar.jpg"
-          replies="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        />
-        
-        <div className="flex-grow"></div> {/* This will push the Navigation to the bottom */}
+
+        <ScrollArea className="flex-grow w-full">
+          <Tell
+            message="message"
+            username="khem"
+            timeAgo="2 hours ago"
+            avatarUrl="/path/to/avatar.jpg"
+            replies="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+          <Tell
+            message="message"
+            username="khem"
+            timeAgo="2 hours ago"
+            avatarUrl="/path/to/avatar.jpg"
+            replies="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+          <Tell
+            message="message"
+            username="khem"
+            timeAgo="2 hours ago"
+            avatarUrl="/path/to/avatar.jpg"
+            replies="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+          <Tell
+            message="message"
+            username="khem"
+            timeAgo="2 hours ago"
+            avatarUrl="/path/to/avatar.jpg"
+            replies="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />
+
+          
+        </ScrollArea>
+
         <Navigation />
       </main>
     </div>

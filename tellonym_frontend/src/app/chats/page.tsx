994902af7bc9249e387fb31747eddfcdbb3024c4
khemraj9815 <02230286.cst@rub.edu.bt>
2024-06-14 17:24:02 +0600

    chat list

---
+++ tellonym_frontend/src/app/chats/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Tell from "../../reuseable_component/hometells"; 
+import Header from "../../reuseable_component/homepageheader";
+import Navigation from "../../reuseable_component/navigation";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import Recommendation from '@/reuseable_component/recommendation';
+
+const HomePage = () => {
+  return (
+    <div className="flex justify-center items-start h-screen bg-gray-100">
+      <main className="flex flex-col items-center justify-start h-full max-w-md w-full p-4 border border-gray-300 bg-white shadow-md rounded-lg">
+        <div>
+          <Header />
+        </div>
+
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;

cac06c6c5b0d116c78b0f50b6a44079a528db2c6
Kinleyjigs <141105750+Kinleyjigs@users.noreply.github.com>
2024-06-15 01:32:24 +0600

    follower_page

---
+++ tellonym_frontend/src/app/follower/page.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from "../../reuseable_component/followers";
+import Navigation from "../../reuseable_component/navigation";
+import Follower from '../../reuseable_component/follower'; // Capitalize the component name
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+
+const HomePage = () => {
+  return (
+    <div className="flex justify-center items-start h-screen bg-gray-100">
+      <main className="flex flex-col items-center justify-between h-full max-w-md w-full p-4 border border-gray-300 bg-white shadow-md rounded-lg space-y-4">
+        <Header />
+        <ScrollArea className="flex-grow w-full">
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+          <Follower /> {/* Use the capitalized component name */}
+        </ScrollArea>
+        <div className="w-full flex justify-center p-4">
+          <Navigation />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;

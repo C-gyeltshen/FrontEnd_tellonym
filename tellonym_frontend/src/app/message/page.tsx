13102afd446db507ca1786c12f07e60797e97e80
Rynorbu <02230297.cst@rub.edu.bt>
2024-06-16 03:36:13 +0600

    input

--- tellonym_frontend/src/app/message/page.tsx
+++ tellonym_frontend/src/app/message/page.tsx
@@ -1,36 +1,31 @@
 import React from 'react';
 
 import { ScrollArea } from "@/components/ui/scroll-area";
-import Message from "../../reuseable_component/message"
-import MessageHeader from '@/reuseable_component/messageHeader'
+import MessageHeader from '@/reuseable_component/messageHeader';
 import ChatMessage from '@/reuseable_component/chatmessage';
+import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
+  return (
+    <div className="flex justify-center items-start h-screen bg-gray-100">
+      <main className="flex flex-col items-center justify-between h-full max-w-md w-full p-4 border">
+        <MessageHeader />
+        
+        <ScrollArea className="flex-grow w-full">
+          <ChatMessage />
+        </ScrollArea>
 
-return (
-
-<div className="flex justify-center items-start h-screen bg-gray-100">
-
-<main className="flex flex-col items-center justify-between h-full max-w-md w-full p-4 border">
-
-<MessageHeader/>
-
-<ScrollArea className="flex-grow w-full">
-<ChatMessage/>
-<Message/>
-
-</ScrollArea>
-
-<div className="w-full flex justify-center p-4">
-
-</div>
-
-</main>
-
-</div>
-
-);
-
+        <div className="w-full flex justify-between items-center space-x-2 p-4">
+          <input
+            type="text"
+            className="flex-grow p-2 border border-gray-300 rounded"
+            placeholder="Type your message..."
+          />
+          <Button variant="outline">Send</Button>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default HomePage;

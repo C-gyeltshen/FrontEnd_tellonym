fb7870c3c859a1c451e68b9c5d150273fb12a36f
khemraj9815 <02230286.cst@rub.edu.bt>
2024-06-17 18:16:43 +0600

    chat header

--- tellonym_frontend/src/reuseable_component/messageHeader.tsx
+++ tellonym_frontend/src/reuseable_component/messageHeader.tsx
@@ -6,23 +6,19 @@ const MessageHeader = () => {
   
    return (
     <main className='flex p-3'>
-  <div className='flex w-full items-center text-xl cursor-pointer justify-between'>
-    <div className='flex items-center justify-between'>
-      <MdOutlineArrowBack />
-    </div>
-    <div className='flex-grow text-center'>
-      <h1>UserName</h1>
-    </div>
-    <div className='flex items-center'>
-      <CgMoreVerticalAlt className='cursor-pointer' />
-    </div>
-  </div>
+      <div className='flex w-full items-center text-xl cursor-pointer justify-between'>
+        <div className='flex items-center justify-between'>
+          <MdOutlineArrowBack />
+        </div>
+        <div className='flex-grow text-center'>
+          <h1>UserName</h1>
+        </div>
+        <div className='flex items-center'>
+          <CgMoreVerticalAlt className='cursor-pointer' />
+        </div>
+      </div>
 </main>
 
-  
-  
-  
-  
   );
 };
 
